de68fee1b558fd9b148845de48d4ac89d8a99e63
Josh Rivera <beps@Mactop.local>
2024-05-15 17:57:39 -0500

    Added model import to fact.py

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,5 +1,6 @@
 #config
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +8,13 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, PetFax! <a href="/pets">Click here for pet index</a>'
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:PostGRE0420@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
     
     from . import (pet, fact)
     app.register_blueprint(pet.bp)
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,5 @@
-from flask import ( Blueprint, render_template, request, redirect) 
+from flask import ( Blueprint, render_template, request, redirect)
+from . import models 
 import json
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
@@ -8,9 +9,14 @@ pets = json.load(open('pets.json'))
 @bp.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
         return redirect('/facts')
-    return render_template('/facts/index.html', pets=pets)
+    return render_template('/facts/index.html')
 
 @bp.route('/new')
 def new(): 
